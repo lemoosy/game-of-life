25d9d5b86f0bb4dbc1e349e943726c3a89411628
Leo976 <leocraft.lemoosy@gmail.com>
2021-02-11 08:46:24 +0100

    count_neighboorn

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
-
-
-
+import time
+import os
+import copy
 
 
 nb_line = 10
@@ -9,6 +9,15 @@ nb_column = 20
 
 
 matrix = [[0 for j in range(nb_column)] for i in range(nb_line)]
+
+matrix[3][1] = 1
+matrix[3][2] = 1
+matrix[3][3] = 1
+
+
+old_matrix = copy.deepcopy(matrix)
+
+
 
 
 
@@ -20,9 +29,43 @@ def print_matrix():
 		print()
 
 
+def count_neighboorn(line, column):
+
+	nb_cells = 0
+
+	for i in range(line - 1, line + 2):
+		for j in range(column - 1, column + 2):
+			if (i != line or j != column) and old_matrix[i][j] != 0:
+				nb_cells += 1
+
+	return nb_cells
+
+
+
+
 
 
 
 print_matrix()
+
+print(count_neighboorn(2,1))
+print(count_neighboorn(3,3))
+
+input()
+
+
+
+
+
+while True:
+
+
+	print_matrix()
+	time.sleep(1)
+	os.system('cls')
+
+
+
+
 
 input()
