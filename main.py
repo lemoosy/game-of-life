4d22cb4a2e9f753057cf71ea4da1998f28a7df4d
Leo976 <leocraft.lemoosy@gmail.com>
2021-02-22 12:56:25 +0100

    Correction du freeze

--- main.py
+++ main.py
@@ -7,23 +7,24 @@ import pygame
 pygame.init()
 window = pygame.display.set_mode((SIZE_WINDOW_X, SIZE_WINDOW_Y))
 matrix = Matrix()
-clock = pygame.time.Clock()
+clock = 0
 
 
 while True:
 
-	for event in pygame.event.get():
+    for event in pygame.event.get():
 
-		if event.type == pygame.QUIT:
-			pygame.quit()
+        if event.type == pygame.QUIT:
+            pygame.quit()
 
-		if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-			position_mouse_x = pygame.mouse.get_pos()[0]
-			position_mouse_y = pygame.mouse.get_pos()[1]
-			matrix.switch_cell(position_mouse_x, position_mouse_y)
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            position_mouse_x = pygame.mouse.get_pos()[0]
+            position_mouse_y = pygame.mouse.get_pos()[1]
+            matrix.switch_cell(position_mouse_x, position_mouse_y)
 
-	window.fill(BLACK)
-	matrix.draw(window)
-	pygame.display.flip()
-	clock.tick(1)
-	matrix.update()
+    if clock != pygame.time.get_ticks() // 1000:
+        window.fill(BLACK)
+        matrix.draw(window)
+        pygame.display.flip()
+        matrix.update()
+        clock = pygame.time.get_ticks() // 1000
