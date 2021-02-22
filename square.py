e916a9b9281d5dffec5313f6962f979f67e5a064
Leo976 <leocraft.lemoosy@gmail.com>
2021-02-22 08:31:43 +0100

    Ajout de la couleur dans __init__

--- square.py
+++ square.py
@@ -4,12 +4,12 @@ import pygame
 
 class Square(pygame.sprite.Sprite):
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, color):
 
         super(Square, self).__init__()
 
         self.surf = pygame.Surface((SIZE_CELL_X, SIZE_CELL_Y))
-        self.surf.fill(RED)
+        self.surf.fill(color)
 
         self.rect = self.surf.get_rect()
         self.rect = self.rect.move(x, y)
