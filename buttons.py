4daa9ca6639e36a36c91451043786f8bc621cea5
Leo976 <leocraft.lemoosy@gmail.com>
2021-02-23 17:32:31 +0100

    Fichier buttons.py terminer

--- buttons.py
+++ buttons.py
@@ -3,36 +3,31 @@ import pygame
 
 
 def draw(window, images):
-
     window.blit(images['stop.png'], POSITION_FIRST_BUTTON)
     window.blit(images['restart.png'], POSITION_SECOND_BUTTON)
     window.blit(images['export.png'], POSITION_THIRD_BUTTON)
 
 
-def click():
-
+def check():
     for event in pygame.event.get():
-
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
 
-            if mouse_on_button_1(*pygame.mouse.get_pos()):
+            mouse_position = pygame.mouse.get_pos()
+
+            if mouse_on_button_1(*mouse_position):
                 return 1
 
-            if mouse_on_button_2(*pygame.mouse.get_pos()):
+            if mouse_on_button_2(*mouse_position):
                 return 2
 
-            if mouse_on_button_3(*pygame.mouse.get_pos()):
+            if mouse_on_button_3(*mouse_position):
                 return 3
-
-            matrix.switch_cell(position_mouse_x, position_mouse_y)
-            button = check_click_button(position_mouse_x, position_mouse_y)
 
         if event.type == pygame.QUIT:
             return 4
 
 
 def mouse_on_button_1(x, y):
-
     if (POSITION_FIRST_BUTTON[0] <= x <= POSITION_FIRST_BUTTON[0] + SIZE_BUTTON_X and
             POSITION_FIRST_BUTTON[1] <= y <= POSITION_FIRST_BUTTON[1] + SIZE_BUTTON_Y):
         return True
@@ -41,17 +36,16 @@ def mouse_on_button_1(x, y):
 
 
 def mouse_on_button_2(x, y):
-
-    if (POSITION_FIRST_BUTTON[0] <= x <= POSITION_FIRST_BUTTON[0] + SIZE_BUTTON_X and
-            POSITION_FIRST_BUTTON[1] <= y <= POSITION_FIRST_BUTTON[1] + SIZE_BUTTON_Y):
+    if (POSITION_SECOND_BUTTON[0] <= x <= POSITION_SECOND_BUTTON[0] + SIZE_BUTTON_X and
+            POSITION_SECOND_BUTTON[1] <= y <= POSITION_SECOND_BUTTON[1] + SIZE_BUTTON_Y):
         return True
 
     return False
 
-def mouse_on_button_3(x, y):
 
-    if (POSITION_FIRST_BUTTON[0] <= x <= POSITION_FIRST_BUTTON[0] + SIZE_BUTTON_X and
-            POSITION_FIRST_BUTTON[1] <= y <= POSITION_FIRST_BUTTON[1] + SIZE_BUTTON_Y):
+def mouse_on_button_3(x, y):
+    if (POSITION_THIRD_BUTTON[0] <= x <= POSITION_THIRD_BUTTON[0] + SIZE_BUTTON_X and
+            POSITION_THIRD_BUTTON[1] <= y <= POSITION_THIRD_BUTTON[1] + SIZE_BUTTON_Y):
         return True
 
     return False
